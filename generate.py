6a6dd917bec681efaf2153939f3285e99b2659db
Rajat Asthana <rajatasthana4@gmail.com>
2018-12-10 14:48:25 +0530

    index generation

--- generate.py
+++ generate.py
@@ -17,16 +17,30 @@ DATE_FORMAT = '%Y-%m-%d'
 
 
 env = Environment(loader=FileSystemLoader(THEME_DIR))
+
 POST_TEMPLATE = env.get_template('post.html')
+INDEX_TEMPLATE = env.get_template('index.html')
+
+
+def _convert_filename(filename, target='.html'):
+	file, ext = os.path.splitext(filename)
+	return '{}{}'.format(file, target)
 
 
 def generate_context(attributes):
-	return {
-		'title': attributes['title'] or SITE_TITLE,
-		'date': datetime.date.strftime(attributes['date'], DATE_FORMAT),
-		'author': attributes['author'] or None,
-		'category': attributes['category'] or None
+	context = {
+		'title': attributes.get('title', SITE_TITLE),
+		'author': attributes.get('author'),
+		'category': attributes.get('category')
 	}
+	try:
+		context.update({
+			'date': datetime.date.strftime(attributes['date'], DATE_FORMAT)
+		})
+	except KeyError:
+		pass
+	return context
+
 
 
 def get_all_posts():
@@ -39,20 +53,34 @@ def get_all_posts():
 				yield entry.name
 
 
-def generate_html(filename, context, post):
-		new_file_name = os.path.splitext(filename)[0] + '.html'
-		context.update({'content': post})
+def generate_html(filename, context, template=POST_TEMPLATE):
+		new_file_name = _convert_filename(filename)
 		open(os.path.join(OUTPUT_DIR, new_file_name), 'w').write(
-			POST_TEMPLATE.render(context)
+			template.render(context)
 		)
 
 
-def parse_post(file):
+def parse_post(file, with_content=True):
 	with open(os.path.join(CONTENT_DIR, file)) as f:
 		whole_file = f.read()
 	yaml_header, content = whole_file.split('---', maxsplit=1)
 	attributes = yaml.load(yaml_header)
+	if not with_content:
+		return attributes
 	return attributes, content
+
+
+def generate_index(files):
+	titles = [
+		{
+			'title': parse_post(file, with_content=False).get('title'),
+			'url': _convert_filename(file)
+		} for file in files
+	]
+
+	context = generate_context({})
+	context.update({'posts': titles})
+	generate_html('index.md', context, INDEX_TEMPLATE)
 
 
 def main():
@@ -60,10 +88,12 @@ def main():
 		os.mkdir(OUTPUT_DIR)
 
 	posts = get_all_posts()
+	generate_index(posts)
 	for post in posts:
 		attributes, content = parse_post(post)
 		content = misaka.html(content)
 		context = generate_context(attributes)
+		context.update({'content': content})
 		generate_html(post, context, content)
 
 
